538c09590dc01f1356a6ec2f1eb7f0420041ea97
ting0525 <98576851+ting0525@users.noreply.github.com>
2022-05-19 23:24:32 +0800

    Add files via upload

--- page-demo/python/main.py
+++ page-demo/python/main.py
@@ -4,7 +4,7 @@ browser = webdriver.Chrome()
  
 browser.get("http://www.baidu.com")
  
-browser.find_element_by_id("kw").send_keys("selenium")  #F12檢視到的www.baidu.com的輸入框的Id是“kw”,通過id找到該元素後，輸入“selenium”字串
-browser.find_element_by_id("su").click()   #F12檢視到的www.baidu.com的submit的Id是“su”，通過id找到該元素後，執行點選事件
+browser.find_element_by_id("kw").send_keys("selenium")  
+browser.find_element_by_id("su").click()   
  
 browser.quit()
